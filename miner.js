3e7676f994195e6feb7b713000fbf9ee3cb8e99c
andytoma <43856136+andytoma@users.noreply.github.com>
2019-02-28 19:48:48 +0300

    Add files via upload

--- miner.js
+++ miner.js
@@ -3,8 +3,12 @@ const MINE_2 = 7;
 const MINE_3 = 9;
 
 var miner    = Math.round(Math.random()*10);
+var miner_health = 5;  //viata
 
 function move_left(){
+  if (miner == MINE_1 || miner == MINE_2 || miner == MINE_3) {
+    miner_health--;
+  }
   if (miner > 1) {
     miner--;
   }else {
@@ -13,41 +17,48 @@ function move_left(){
   show()
 }
 function move_right(){
+  if (miner == MINE_1 || miner == MINE_2 || miner == MINE_3){
+    miner_health--;
+  }
   if (miner < 10) {
     miner++;
   }else {
   miner = 1;
   }
+
   show()
+
 }
+
 function show(){
-  var div = document.getElementById('map'); // conectam variabila du ID
-  div.innerHTML = ``;  // curatam totul ce era pina acum in div
-for (var step = 1 ; step<=10; step++){
-  if ( step == miner) {
-    var content = `<div class="miner"></div>`;   // template string
-  }else {
+  var div = document.getElementById('map'); //conectam div cu elem
+    div.innerHTML=``;  //curatam divul
+                 //!!!!!!!!MAP!!!!!!!!!!!!//
+for(var step = 1; step<=10; step++ ){
+    if(step == miner){
+  var content =`<div class="miner"></div>`; //template string
+ }else {
     var content = ``;
+ }
+                  //!!!!!!!!BOMB!!!!!!!!!!!!//
+  if(step == MINE_1){
+  var content_2 = `<div class="bomb"></div>`
+  }else {
+  var content_2 = ``;
   }
-  div.innerHTML +=
-    `<div class="grass">
-      ${content}
-    </div>`
-  ;
+  if(step == MINE_2){
+  var content_3 = `<div class="bomb"></div>`
+  }else {
+  var content_3 = ``;
+  }if(step == MINE_3){
+  var content_4 = `<div class="bomb"></div>`
+  }else {
+  var content_4 = ``;
+  }
+  div.innerHTML += `<div class="grass"> ${content} ${content_2}${content_3}${content_4}</div>`;
 
 }
-for (var step = 1 ; step<=10; step++){
-  if ( step == bomb) {
-    var content = `<div class="bomb"></div>`;   // template string
-  }else {
-    var content = ``;
-  }
-  div.innerHTML +=
-    `<div class="bomb">
-      ${content}
-    </div>`
-  ;
-
-}
+var div_s = document.getElementById('score');
+div_s.innerHTML ="Viata:= " +miner_health;
 }
 show();
